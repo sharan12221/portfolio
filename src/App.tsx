10d4ac6f32d43ac2767b6738487fe034431b2898
Sharan Wakade <117678416+sharan12221@users.noreply.github.com>
2025-01-26 02:42:16 +0530

    added track

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Experience } from './components/Experience';
 import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 import { Contact } from './components/Contact';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
